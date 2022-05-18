5923f3a9c56aaddc13e3a46492c478cca186983f
Chaster Kim <chaster@bzznbyd.com>
2022-05-18 18:42:49 +0900

    클레이 전송 기능 추가

--- src/components/Wallet/WalletPage.js
+++ src/components/Wallet/WalletPage.js
@@ -2,12 +2,24 @@ import React, { useState, useEffect } from 'react';
 import './WalletPage.css';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
+import Modal from 'react-modal';
 
 export default function WalletPage() {
   const currentUser = useSelector(state => state.currentUser);
   const userId = currentUser.user;
   const [isWalletCreated, setIsWalletCreated] = useState(false);
   const [walletInfo, setWalletInfo] = useState({});
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [input, setInput] = useState({
+    toAddress: "",
+    amount: "",
+  });
+
+  const onChangeHandler = (e) => {
+    setInput((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_BACKEND_HOST}/users`)
@@ -52,6 +64,23 @@ export default function WalletPage() {
       .catch(err => console.error(err));
   };
 
+  const onClickModalOpen = () => {
+    setModalIsOpen(true);
+  };
+
+  const onClickSendKlay = () => {
+    axios.post(`${process.env.REACT_APP_BACKEND_HOST}/sendKlay`, {
+      fromAddress: walletInfo.address,
+      toAddress: input.toAddress,
+      amount: input.amount
+    })
+    .then(res => {
+      alert(res.data.msg);
+      setModalIsOpen(false);
+    })
+    .catch(err => console.error(err));
+  }
+
   return (
     <div className='wallet-page'>
       {!isWalletCreated &&
@@ -68,10 +97,43 @@ export default function WalletPage() {
             <p>주소: {walletInfo.address}</p>
             <p>네트워크: {walletInfo.chainId === 1001 ? 'Baobab' : 'Main'}</p>
             <p>생성일자: {new Date(walletInfo.createdAt * 1000).toISOString().slice(0, 10)}</p>
-            <p className='check-balance' onClick={onClickCheckBalance}>내 KLAY 확인하기</p>
+            <div className='wallet-options'>
+              <p onClick={onClickCheckBalance}>내 KLAY 확인하기</p>
+              <p onClick={onClickModalOpen}>KLAY 전송하기</p>
+              <p>NFT 발행하기</p>
+            </div>
           </div>
         </div>
       }
+      <Modal
+        isOpen={modalIsOpen}
+        appElement={document.getElementById('root')}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(255, 255, 255, 0.5)'
+          },
+          content: {
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '400px',
+            height: '350px',
+            background: '#fff',
+            overflow: 'auto',
+            WebkitOverflowScrolling: 'touch',
+            borderRadius: '4px',
+            boxShadow: '0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24)'
+          }
+        }}
+      >
+        <div className='klay-form'>
+          <input name='toAddress' type='text' placeholder='KLAY를 보낼 주소' onChange={onChangeHandler} />
+          <input name='amount' type='text' placeholder='보낼 KLAY 양' onChange={onChangeHandler} />
+          <button className='send-button' onClick={onClickSendKlay}>전송하기</button>
+        </div>
+      </Modal>
     </div>
   );
 }
